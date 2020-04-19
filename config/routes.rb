6667171523dc0c58d9c56949c71a4800716c5446
takeshi075 <real.feet15@gmail.com>
2020-04-19 13:19:14 +0900

    ユーザー管理機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< Updated upstream
   root "groups#index"
   resources :users, only: [:edit, :update]
   resources :groups, only: [:index,:new,:create, :edit, :update] do
     resources :messages,only: [:index, :create]
   end
-=======
-  root "messages#index"
->>>>>>> Stashed changes
 end
